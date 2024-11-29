86cc8b7c2b27f6836411b511ad57feba5f19f711
Alix Leve <alix.leve@gmail.com>
2024-11-29 16:13:41 +0100

    add: dict for class Player and function to init dict in class Game

--- awele_POO.py
+++ awele_POO.py
@@ -2,9 +2,13 @@ class Player():
     def __init__(self, player_name):
         self.player_name = player_name
         self.__player_score = 0
+        self.player_board = {}
 
     def get_player_name(self):
         return self.player_name   
+    
+    def get_player_board(self):
+        return self.player_board
 
     def display_player_info(self):
         print(f"{self.player_name} has a score of {self.__player_score} points.")
@@ -12,54 +16,87 @@ class Player():
 
 class Game():
     def __init__(self, player1_name, player2_name):
-        self.letters = " a  b  c  d  e  f g  h  i  j  k  l"
-        self.board = [4,4,4,4,2,2,3,2,4,4,4,4]
+        self.letters_to_display = " a  b  c  d  e  f  g  h  i  j  k  l "
+        self.letters_to_dict = "abcdefghijkl"
+        self.board = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
         self.start_number_seeds = 48
         self.score = 0
         self.player1 = Player(player1_name)
         self.player2 = Player(player2_name)
-        self.current_player = self.player1
-        self.index = 7
+        self.current_player = self.player2
+        self.index = 0
+
 
     def display_player_name(self):
         return f"{self.current_player.get_player_name()}"
 
+
     def display_number_of_seeds(self):
         print(f"Remaing seeds: {self.__start_number_seeds}")
+
 
     def display_board(self):
         length_board = len(self.board)
         middle_board = int(length_board / 2)
-        first_part_board = self.board[:middle_board]
-        second_part_board = self.board[middle_board:]
-        second_part_board.reverse()
+        self.first_part_board = self.board[:middle_board]
+        self.second_part_board = self.board[middle_board:]
+        self.second_part_board.reverse()
 
-        length_letters = len(self.letters)
+        length_letters = len(self.letters_to_display)
         middle_letters = int(length_letters / 2)
-        first_part_letters = self.letters[:middle_letters]
-        second_part_letters = self.letters[middle_letters:]
-        second_part_board.reverse()
+        self.first_part_letters = self.letters_to_display[:middle_letters]
+        self.second_part_letters = self.letters_to_display[middle_letters:]
+        self.second_part_letters = self.second_part_letters[::-1]
 
-        print(second_part_letters)
-        print(second_part_board)
-        print(first_part_board)
-        print(first_part_letters)
+        print(self.second_part_letters)
+        print(self.second_part_board)
+        print(self.first_part_board)
+        print(self.first_part_letters)
+
+        return self.first_part_board, self.second_part_board
+
+    # REFACTO A FAIRE player2_board_init et player1_board_init
+    def player1_board_init(self):
+        length_letters = len(self.letters_to_dict)
+        middle_letters = int(length_letters / 2)
+        self.first_part_letters_dict = self.letters_to_dict[:middle_letters]
+        self.second_part_letters_dict = self.letters_to_dict[middle_letters:]
+
+        self.player_board = self.player1.get_player_board()
+        for key, value in zip(self.second_part_letters_dict, self.second_part_board):
+            self.player_board[key] = value
+        print(self.player_board)
+
+
+    def player2_board_init(self):
+        self.player_board = self.player2.get_player_board()
+        for key, value in zip(self.first_part_letters_dict, self.first_part_board):
+            self.player_board[key] = value
+        print(self.player_board)
+
 
     def getting_user_index_for_saw(self):
         self.index = input("Sélectionnez le champs depuis lequel vous voulez semer: ")
+        if self.current_player == self.player1 and int(self.index) > 5:
+            self.index = input("Vous devez sélectionner un champ compris entre les lettres a et f incluses: ")
+        elif self.current_player == self.player2 and int(self.index) < 6:
+            self.index = input("Vous devez sélectionner un champ compris entre les lettres g et l incluses: ")   
         self.index = int(self.index)
         return self.index
-    
+
+
     def getting_user_index_for_harvest(self):
         self.index = input("Sélectionnez le champs à partir duquel vous voulez récoltez: ")
         self.index = int(self.index)
         return self.index
-    
+
+
     def get_next_index_saw(self):
         if self.index == len(self.board) -1:
             return 0
         else:
             return self.index +1
+
 
     def get_next_index_harvest(self):
         if self.index == 0:
@@ -67,27 +104,18 @@ class Game():
         else:
             return self.index - 1
 
+
     def harvest(self):
         self.score = 0
-        print("board[index]: ", self.board[self.index])
         while self.board[self.index] >=2 and self.board[self.index] <= 3:
-            print("IN WHILE LOOP")
             self.score += self.board[self.index]
-            print("score: ", self.score)
             self.board[self.index] = 0
-            print("board: ", self.board)
             self.next_index = self.get_next_index_harvest()
-            print("next index: ", self.next_index)
             self.index = self.next_index
-            print("index: ", self.index)
         self.start_number_seeds -= self.score 
-        print("start number of seeds: ", self.start_number_seeds)
         print(f"You're score: {self.score}")    
 
-    #TO DO 
-    #en fonction du joueur en train de jouer, délimiter la partie du plateau à partir de laquelle il peut jouer
-    #joueur1 = plateau[0] à plateau[5]
-    #joueur2 = plateau[6] à plteau[11]
+
     def saw(self):
         self.number_of_seeds = self.board[self.index]
         self.board[self.index] = 0
@@ -100,20 +128,14 @@ class Game():
             self.index = self.next_index  
 
 
-
     def start_turn(self):
         while self.start_number_seeds > 0:
-            # print("Le plateau de départ : [3, 4, 3, 2, 7, 8, 7, 3, 3, 7, 5, 3]")
             awele.display_board()
-            print(f"C'est au joueur {self.display_player_name()} de jouer")
-            # print("Un input pour demander sur quelle case joueur commence: [index]")
+            print(f"C'est au tour de {self.display_player_name()} de jouer")
             awele.getting_user_index_for_saw()
-            # print("on récupère [index] et on exécute saw(index)")
             awele.saw()
             awele.display_board()
-            # print("Input : demande au joueur à partir de quelle case iel veut ramasser graines: [index]")
             awele.getting_user_index_for_harvest()
-            # print("on appelle harvest(index)")
             awele.harvest()
             if self.current_player == self.player1:
                 self.current_player = self.player2
@@ -126,3 +148,9 @@ class Game():
 awele = Game("Ada", "Audrey")
 awele.start_turn()
 
+
+    #TO DO 
+    # fonction pour relier la lettre entrée dans input et l'index correspondant
+    #en fonction du joueur en train de jouer, délimiter la partie du plateau à partir de laquelle il peut jouer
+    #joueur1 = plateau[0] à plateau[5]
+    #joueur2 = plateau[6] à plteau[11]
